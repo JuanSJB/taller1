f4c0b307d2f988f99179a810f4d9b366570832ad
JuanSJB <jsjacomeb@eafit.edu.co>
2025-02-05 10:38:37 -0500

    primer commit

---
+++ Holiwi1/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Holiwi1Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Holiwi1'
---
+++ Holiwi1/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def home(request):
+    #return render(request, 'home.html')
+    return render(request, "home.html",{'name': "Cool Name"})
+def about(request):
+    return HttpResponse('<h1>Welcome to about</h1>')
