df7a88f1db70a0290b62690724fbc36b503372b1
JuanSJB <jsjacomeb@eafit.edu.co>
2025-02-05 11:12:16 -0500

    actualizacion archivo views.py

--- Holiwi1/views.py
+++ Holiwi1/views.py
@@ -5,4 +5,4 @@ def home(request):
     #return render(request, 'home.html')
     return render(request, "home.html",{'name': "Juan Jacome"})
 def about(request):
-    return HttpResponse('<h1>Welcome to about</h1>')
+    return render(request,'about.html')
