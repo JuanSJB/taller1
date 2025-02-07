7477890b40c9b227921d5f900518625f11e7851e
JuanSJB <jsjacomeb@eafit.edu.co>
2025-02-07 15:05:31 -0500

    Search movies

--- Holiwi1/views.py
+++ Holiwi1/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import movie
 # Create your views here.
 def home(request):
     #return render(request, 'home.html')
-    return render(request, "home.html",{'name': "Juan Jacome"})
+    #return render(request, "home.html",{'name': "Juan Jacome"})
+    searchTerm = request.GET.get('searchMovie')
+    if  searchTerm:
+        movies = movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = movie.objects.all()
+    return render(request,'home.html',{'searchterm': searchTerm, 'movies': movies})
+
+
 def about(request):
     return render(request,'about.html')
